f6796578701a4d3beb19f23cdd0d9f3b985ac318
ArCaneSec <arcanebughunter@gmail.com>
2024-04-24 21:06:30 +0330

    cod cleaning in reload method, timeline now also has refresh button when it didnt receive data from request.

--- frontend/src/app/(authenticated)/[username]/page.js
+++ frontend/src/app/(authenticated)/[username]/page.js
@@ -1,5 +1,8 @@
 "use client";
-import { LoadingSpinner } from "@/app/(authenticated)/components";
+import {
+  LoadingSpinner,
+  RefreshButton,
+} from "@/app/(authenticated)/components";
 import fetchRequest from "@/app/(authenticated)/utils";
 import { _BACK_URL, apis, urls } from "@/app/urls";
 import Image from "next/image";
@@ -16,30 +19,37 @@ export default function userPreview({ params }) {
   const router = useRouter();
 
   const getData = async () => {
-    const res = await fetchRequest(
-      `${apis.userData}${params.username}/?page=${page}`,
-      "GET"
-    );
-    const data = await res.json();
-    if (res.ok) {
-      setUserData(data);
-      setPosts([...posts, ...data.posts]);
-      setPage(page + 1);
-    } else if (res.redirected) {
-      router.push(urls.login);
-      return;
-    } else if (res.status === 404 && data.error === "emptyPage") {
-      const button = document.getElementById("reload-button");
-      const spinner = document.getElementById("ref");
-      button && button.remove();
-      spinner && spinner.remove();
-    } else if (res.status === 404 && data.detail === "Not found.") {
-      router.push(urls.dashboard);
-      return;
-    } else {
+    try {
+      const res = await fetchRequest(
+        `${apis.userData}${params.username}/?page=${page}`,
+        "GET"
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserData(data);
+        setPosts([...posts, ...data.posts]);
+        setPage(page + 1);
+      } else if (res.redirected) {
+        router.push(urls.login);
+        return;
+      } else if (res.status === 404 && data.error === "emptyPage") {
+        const button = document.getElementById("loading");
+        button && button.remove();
+      } else if (res.status === 404 && data.detail === "Not found.") {
+        router.push(urls.dashboard);
+        return;
+      } else {
+        setReloadRequired(true);
+      }
+    } catch (e) {
+      console.log(e);
       setReloadRequired(true);
-      document.getElementById("ref").classList.add("hidden");
     }
+  };
+
+  const refresh = () => {
+    setReloadRequired(false);
+    getData();
   };
 
   useEffect(() => {
@@ -47,7 +57,7 @@ export default function userPreview({ params }) {
       console.log(`fetching posts again, page number ${page}`);
       getData();
     }
-  }, [inView, page, reloadRequired]);
+  }, [inView, page]);
 
   return (
     <div>
@@ -63,39 +73,12 @@ export default function userPreview({ params }) {
         </div>
       </div>
       <UserPosts posts={posts} />
-      <div className="ml-72">
-        <div
-          id="ref"
-          ref={(e) => !reloadRequired && ref(e)}
-          className="justify-center pt-2"
-        >
+      <div className="relative flex justify-center" ref={ref} id="loading">
+        {reloadRequired ? (
+          <RefreshButton refresh={refresh} />
+        ) : (
           <LoadingSpinner />
-        </div>
-        <button
-          className={`${
-            reloadRequired ? "visible" : "hidden"
-          } flex items-center px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-indigo-600 rounded-lg hover:bg-indigo-500 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-80`}
-          onClick={() => {
-            setReloadRequired(false);
-            document.getElementById("ref").classList.remove("hidden");
-          }}
-          id="reload-button"
-        >
-          <svg
-            className="w-5 h-5 mx-1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z"
-              clip-rule="evenodd"
-            />
-          </svg>
-
-          <span class="mx-1">Refresh</span>
-        </button>
+        )}
       </div>
     </div>
   );
--- frontend/src/app/(authenticated)/components.js
+++ frontend/src/app/(authenticated)/components.js
@@ -10,15 +10,21 @@ import fetchRequest from "./utils";
 export default function Timeline() {
   const [timeLinePosts, setTimeLinePosts] = useState([]);
   const [ref, inView] = useInView(1);
+  const [reloadRequired, setReloadRequired] = useState(false);
   const router = useRouter();
 
   const fetchRequests = async () => {
-    const res = await fetchRequest(apis.timeline, "GET");
-    if (!res.ok) {
-      router.push(urls.login);
-      return;
+    try {
+      const res = await fetchRequest(apis.timeline, "GET");
+      if (!res.ok) {
+        router.push(urls.login);
+        return;
+      }
+      return await res.json();
+    } catch (e) {
+      setReloadRequired(true);
+      console.log("request timed out");
     }
-    return await res.json();
   };
 
   const fetchTimeline = async () => {
@@ -35,6 +41,12 @@ export default function Timeline() {
     }
   };
 
+  const refresh = () => {
+    console.log("refresh called");
+    setReloadRequired(false);
+    fetchTimeline();
+  };
+
   useEffect(() => {
     if (inView) {
       console.log("fetching time line once again");
@@ -46,7 +58,11 @@ export default function Timeline() {
     <main id="timeline" className="flex flex-col items-center">
       <TimelinePosts posts={timeLinePosts} />
       <div ref={ref} className="justify-center pt-2">
-        <LoadingSpinner />
+        {reloadRequired ? (
+          <RefreshButton refresh={refresh} />
+        ) : (
+          <LoadingSpinner />
+        )}
       </div>
     </main>
   );
@@ -166,5 +182,32 @@ export function LoadingSpinner() {
       </svg>
       <span className="sr-only">Loading...</span>
     </div>
+  );
+}
+
+export function RefreshButton({ refresh }) {
+  return (
+    <button
+      className={
+        "flex items-center px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-indigo-600 rounded-lg hover:bg-indigo-500 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-80"
+      }
+      onClick={refresh}
+      id="reload-button"
+    >
+      <svg
+        className="w-5 h-5 mx-1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z"
+          clip-rule="evenodd"
+        />
+      </svg>
+
+      <span class="mx-1">Refresh</span>
+    </button>
   );
 }
